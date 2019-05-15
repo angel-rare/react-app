912ef28beb28cbc84383ccba0bc97dc2da2f1491
angelrare <g.angelrarae@gmail.com>
2019-05-15 07:07:07 -0700

    add Header_Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome
-        </p>
-      </header>
-    </div>
-  );
+import Header from './Header'
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome
+          </p>
+          <button>Title</button>
+          <Header title="Custom Header"/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
